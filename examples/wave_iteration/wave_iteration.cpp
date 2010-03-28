2976e8c96b34105716a6b4786276942fe704bd78
Zbigniew Siciarz <antyqjon@gmail.com>
2010-03-28 18:53:30 +0200

    Added three iteration examples for WaveFile class.

--- examples/wave_iteration/wave_iteration.cpp
+++ examples/wave_iteration/wave_iteration.cpp
@@ -1,4 +1,6 @@
+#include "aquila/global.h"
 #include "aquila/source/WaveFile.h"
+#include <algorithm>
 #include <cstdlib>
 #include <iostream>
 
@@ -21,6 +23,33 @@ int main(int argc, char *argv[])
     }
 
     Aquila::WaveFile wav(argv[1]);
+    std::cout << "Loaded file: " << wav.getFilename()
+              << " (" << wav.getBitsPerSample() << "b)" << std::endl;
+    Aquila::SampleType maxValue = 0, minValue = 0;
+
+    // simple index-based iteration
+    for (std::size_t i = 0; i < wav.getSamplesCount(); ++i)
+    {
+        if (wav.sample(i) > maxValue)
+            maxValue = wav.sample(i);
+    }
+    std::cout << "Maximum sample value: " << maxValue << std::endl;
+
+    // iterator usage
+    for (Aquila::WaveFile::iterator it = wav.begin(); it != wav.end(); ++it)
+    {
+        if (*it < minValue)
+            minValue = *it;
+    }
+    std::cout << "Minimum sample value: " << minValue << std::endl;
+
+    // STL algorithms work too, so the previous examples could be rewritten
+    // using max/min_element.
+    int limit = 5000;
+    int aboveLimit = std::count_if(wav.begin(), wav.end(),
+                                   std::bind2nd(std::greater<int>(), limit));
+    std::cout << "There are " << aboveLimit << " samples greater than "
+              << limit << std::endl;
 
     return 0;
 }
