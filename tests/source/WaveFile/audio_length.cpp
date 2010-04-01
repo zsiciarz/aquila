bc097e0fe8c3e0a485b79f8f552149fb132bbf41
Zbigniew Siciarz <antyqjon@gmail.com>
2010-04-01 16:59:59 +0200

    Added an audio length unit test.

---
+++ tests/source/WaveFile/audio_length.cpp
@@ -0,0 +1,12 @@
+#include "aquila/source/WaveFile.h"
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    if (argc < 2)
+        return 1;
+
+    Aquila::WaveFile wav(argv[1]);
+    std::cout << wav.getAudioLength();
+    return 0;
+}
