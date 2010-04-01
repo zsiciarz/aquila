d9c3df4a4b4aafbc424292f41b0b7a4ad5ccb253
Zbigniew Siciarz <antyqjon@gmail.com>
2010-04-01 16:42:30 +0200

    Added two more unit tests for WaveFile.

---
+++ tests/source/WaveFile/wave_size.cpp
@@ -0,0 +1,13 @@
+#include "aquila/source/WaveFile.h"
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    if (argc < 2)
+        return 1;
+
+    Aquila::WaveFile wav(argv[1]);
+    std::cout << wav.getWaveSize();
+    return 0;
+}
+
