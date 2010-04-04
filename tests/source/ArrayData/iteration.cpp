676245c04565967cd181f3a51363bbf0e1daeceb
Zbigniew Siciarz <antyqjon@gmail.com>
2010-04-04 14:17:03 +0200

    Changed the iteration test to do what it's supposed to do.

--- tests/source/ArrayData/iteration.cpp
+++ tests/source/ArrayData/iteration.cpp
@@ -1,11 +1,16 @@
+#include "aquila/global.h"
 #include "aquila/source/ArrayData.h"
+#include <algorithm>
 #include <iostream>
+#include <boost/lambda/lambda.hpp>
+
+using namespace boost::lambda;
 
 int main(int argc, char *argv[])
 {
     const int SIZE = 10;
-    double testArray[SIZE] = {0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0};
-    Aquila::ArrayData<double> data(testArray, SIZE, 22050);
-    std::cout << data.getSamplesCount();
+    Aquila::SampleType testArray[SIZE] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    Aquila::ArrayData<> data(testArray, SIZE, 22050);
+    std::for_each(data.begin(), data.end(), std::cout << 2 * _1);
     return 0;
 }
