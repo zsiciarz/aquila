f6e3412bf5d825c1aaf80f992b398c00e9d71b76
Zbigniew Siciarz <antyqjon@gmail.com>
2010-04-05 13:12:19 +0200

    Updated the frame_iteration example.

--- examples/frame_iteration/frame_iteration.cpp
+++ examples/frame_iteration/frame_iteration.cpp
@@ -4,19 +4,39 @@
 #include "aquila/source/FramesCollection.h"
 #include <algorithm>
 #include <cstdlib>
+#include <ctime>
 #include <iostream>
+#include <numeric>
 
 int main(int argc, char *argv[])
 {
+    // seed the random number generator
+    std::srand(std::time(0));
     const int SIZE = 1024, FRAME_SIZE = 127;
+
+    // create an array and fill it with random numbers
     double* testArray = new double[SIZE];
+    std::generate_n(testArray, SIZE, std::rand);
+
+    // wrap the array with SignalSource-based class and divide it into frames
     Aquila::ArrayData<double> data(testArray, SIZE, 44100);
     Aquila::FramesCollection frames(data, FRAME_SIZE);
     std::cout << "Signal size: " << SIZE
               << ", frame size: " << FRAME_SIZE
               << ", number of frames: " << frames.count() << std::endl;
 
-    delete testArray;
+    typedef Aquila::FramesCollection::iterator FrameIterator;
+    unsigned int i = 0;
+    // here, begin() and end() return iterators that point to frame objects
+    for (FrameIterator it = frames.begin(); it != frames.end(); ++it, ++i)
+    {
+        // and here the iterators returned point to individual samples
+        // in each frame
+        double sum = std::accumulate(it->begin(), it->end(), 0.0);
+        std::cout << "\nFrame #" << i << ", sum = " << sum;
+    }
+
+    delete [] testArray;
     return 0;
 }
 
