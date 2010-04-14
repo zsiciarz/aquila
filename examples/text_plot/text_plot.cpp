0a11d1e06f33acfb51f5b1aa28774675e8d1eeff
Zbigniew Siciarz <antyqjon@gmail.com>
2010-04-14 16:15:10 +0200

    Removed ArrayData from text_plot example.

--- examples/text_plot/text_plot.cpp
+++ examples/text_plot/text_plot.cpp
@@ -1,7 +1,5 @@
-#include "aquila/source/ArrayData.h"
 #include "aquila/tools/TextPlot.h"
 #include <cstddef>
-#include <cmath>
 
 int main(int argc, char *argv[])
 {
@@ -12,9 +10,8 @@ int main(int argc, char *argv[])
         arr[i] = i % 10;
     }
 
-    Aquila::ArrayData<int> data(arr, SIZE, 44100);
     Aquila::TextPlot plot("Example plot");
-    plot.plot(data);
+    plot.plot(arr, SIZE);
 
     return 0;
 }
