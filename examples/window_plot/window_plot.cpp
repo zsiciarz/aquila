480970e6acebbad4095adc3bae7f408d2321fd35
Zbigniew Siciarz <antyqjon@gmail.com>
2010-04-14 12:35:52 +0200

    Simplified the window_plot example by using Aquila::TextPlot.

--- examples/window_plot/window_plot.cpp
+++ examples/window_plot/window_plot.cpp
@@ -1,13 +1,5 @@
 #include "aquila/transform/Window.h"
-#include <algorithm>
-#include <iostream>
-#include <iterator>
-#include <vector>
-
-// I know, globals are evil ;)
-std::size_t length = 65, scale = 16;
-
-void plotWindow(const char* name, Aquila::WindowType type, std::size_t length);
+#include "aquila/tools/TextPlot.h"
 
 int main(int argc, char *argv[])
 {
@@ -17,47 +9,16 @@ int main(int argc, char *argv[])
     const char* names[] = {"Rectangular", "Hamming",
                            "Hann", "Barlett",
                            "Blackman", "Flat-top"};
+    const std::size_t length = 65;
     int num = sizeof(types) / sizeof(Aquila::WindowType);
 
+    Aquila::TextPlot plot;
     for (int i = 0; i < num; ++i)
     {
-        plotWindow(names[i], types[i], length);
+        Aquila::Window window(types[i], length);
+        plot.setTitle(names[i]);
+        plot.plot(window.getData());
     }
 
     return 0;
-}
-
-/**
- * Draws a simple text-mode plot of a window.
- *
- * @param name window function name (plot title)
- * @param type window type
- * @param length window length
- */
-void plotWindow(const char* name, Aquila::WindowType type, std::size_t length)
-{
-    std::cout << "\n" << name << " window:\n";
-
-    Aquila::Window window(type, length);
-    const Aquila::Window::WindowDataType& data = window.getData();
-    double max = *std::max_element(data.begin(), data.end());
-
-    std::vector< std::vector<char> > plot(length);
-    for (unsigned int i = 0; i < length; ++i)
-    {
-        std::vector<char> column(scale + 2, ' ');
-        std::size_t y = scale -
-                        static_cast<std::size_t>((scale - 1) * data[i] / max);
-        column[y] = '*';
-        plot[i] = column;
-    }
-
-    for (unsigned int i = 0; i < scale + 2; ++i)
-    {
-        for (unsigned int j = 0; j < length; ++j)
-        {
-            std::cout << plot[j][i];
-        }
-        std::cout << "\n";
-    }
 }
