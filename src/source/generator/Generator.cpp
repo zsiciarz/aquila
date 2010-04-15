8360c7de057ad4b4ae044787ff14cf6ecf211511
Zbigniew Siciarz <antyqjon@gmail.com>
2010-04-15 11:14:40 +0200

    Added a class skeleton for signal generator interface.

---
+++ src/source/generator/Generator.cpp
@@ -0,0 +1,25 @@
+/**
+ * @file Generator.cpp
+ *
+ * An interface for signal generators.
+ *
+ * This file is part of the Aquila DSP library.
+ * Aquila is free software, licensed under the MIT/X11 License. A copy of
+ * the license is provided with the library in the LICENSE file.
+ *
+ * @package Aquila
+ * @version 3.0.0-dev
+ * @author Zbigniew Siciarz
+ * @date 2007-2010
+ * @license http://www.opensource.org/licenses/mit-license.php MIT
+ * @since 3.0.0
+ */
+
+#include "Generator.h"
+
+namespace Aquila
+{
+    Generator::Generator()
+    {
+    }
+}
---
+++ src/source/generator/Generator.h
@@ -0,0 +1,30 @@
+/**
+ * @file Generator.h
+ *
+ * An interface for signal generators.
+ *
+ * This file is part of the Aquila DSP library.
+ * Aquila is free software, licensed under the MIT/X11 License. A copy of
+ * the license is provided with the library in the LICENSE file.
+ *
+ * @package Aquila
+ * @version 3.0.0-dev
+ * @author Zbigniew Siciarz
+ * @date 2007-2010
+ * @license http://www.opensource.org/licenses/mit-license.php MIT
+ * @since 3.0.0
+ */
+
+#ifndef GENERATOR_H
+#define GENERATOR_H
+
+namespace Aquila
+{
+    class Generator
+    {
+    public:
+        Generator();
+    };
+}
+
+#endif // GENERATOR_H
