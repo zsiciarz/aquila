810a036fe2ca80afc8082bdf1a2eb699b5c4aa2d
Zbigniew Siciarz <antyqjon@gmail.com>
2010-05-15 12:57:21 +0200

    Updated the sine generator example tu use new Generator API.

--- examples/sine_generator/sine_generator.cpp
+++ examples/sine_generator/sine_generator.cpp
@@ -3,9 +3,13 @@
 
 int main(int argc, char *argv[])
 {
-    Aquila::SineGenerator generator(1000, 64);
-    generator.setFrequency(32).setAmplitude(255).generate();
+    Aquila::SineGenerator generator(1000);
+    generator.setFrequency(32).setAmplitude(255).generate(64);
     Aquila::TextPlot plot("Sine wave");
+    plot.plot(generator);
+
+    // resize the generator
+    generator.setFrequency(50).generate(32);
     plot.plot(generator);
 
     return 0;
