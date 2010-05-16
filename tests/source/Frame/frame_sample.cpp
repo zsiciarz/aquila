ecd62510561de0ce641856fc07bc6dade6d87a75
Zbigniew Siciarz <antyqjon@gmail.com>
2010-05-16 14:48:01 +0200

    Added a few tests for per-sample data access in Frame class.

---
+++ tests/source/Frame/frame_sample.cpp
@@ -0,0 +1,31 @@
+#include "aquila/global.h"
+#include "aquila/source/ArrayData.h"
+#include "aquila/source/Frame.h"
+#include <cstdlib>
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    const int SIZE = 10;
+    Aquila::SampleType testArray[SIZE] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    Aquila::ArrayData<> data(testArray, SIZE, 22050);
+
+    int from = 0, to = 1, pos = 0;
+    if (argc > 1)
+    {
+        from = std::atoi(argv[1]);
+    }
+    if (argc > 2)
+    {
+        to = std::atoi(argv[2]);
+    }
+    if (argc > 3)
+    {
+        pos = std::atoi(argv[3]);
+    }
+    Aquila::Frame frame(data, from, to);
+    std::cout << frame.sample(pos);
+
+    return 0;
+}
+
