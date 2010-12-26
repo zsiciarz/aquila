df8a3966ea67fbf0de1cc76bc19e6fa9800e8913
Zbigniew Siciarz <antyqjon@gmail.com>
2010-12-26 11:27:03 +0100

    Added a SoundBufferAdapter class.
    This is an SFML-specific wrapper around SignalSource to use whenever
    sf::SoundBuffer is neccessary and you have an instance of a class derived from
    SignalSource.

---
+++ src/wrappers/SoundBufferAdapter.h
@@ -0,0 +1,42 @@
+/**
+ * @file SoundBufferAdapter.h
+ *
+ * A wrapper around SignalSource to use as a sound buffer in SFML.
+ *
+ * This file is part of the Aquila DSP library.
+ * Aquila is free software, licensed under the MIT/X11 License. A copy of
+ * the license is provided with the library in the LICENSE file.
+ *
+ * @package Aquila
+ * @version 3.0.0-dev
+ * @author Zbigniew Siciarz
+ * @date 2007-2010
+ * @license http://www.opensource.org/licenses/mit-license.php MIT
+ * @since 3.0.0
+ */
+
+#ifndef SOUNDBUFFERADAPTER_H
+#define SOUNDBUFFERADAPTER_H
+
+#include "../global.h"
+#include "../source/SignalSource.h"
+#include <SFML/Audio.hpp>
+
+namespace Aquila
+{
+    /**
+     * A wrapper around SignalSource to use as a sound buffer in SFML.
+     */
+    class AQUILA_EXPORT SoundBufferAdapter : public sf::SoundBuffer
+    {
+    public:
+        SoundBufferAdapter();
+        SoundBufferAdapter(const SoundBufferAdapter& other);
+        SoundBufferAdapter(const SignalSource& source);
+        ~SoundBufferAdapter();
+
+        bool LoadFromSignalSource(const SignalSource& source);
+    };
+}
+
+#endif // SOUNDBUFFERADAPTER_H
