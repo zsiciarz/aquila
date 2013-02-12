6c17dac15c35b6c917ffb5f1478d27c26bb03492
Zbigniew Siciarz <antyqjon@gmail.com>
2013-02-12 19:58:40 +0100

    More tests for FrameCollection.

--- tests/source/FramesCollection.cpp
+++ tests/source/FramesCollection.cpp
@@ -15,4 +15,40 @@ SUITE(FramesCollection)
         Aquila::FramesCollection frames;
         CHECK_EQUAL(0, frames.count());
     }
+
+    TEST(FiveSamplesPerFrame)
+    {
+        Aquila::FramesCollection frames(data, 5);
+        CHECK_EQUAL(2, frames.count());
+    }
+
+    TEST(TwoSamplesPerFrame)
+    {
+        Aquila::FramesCollection frames(data, 2);
+        CHECK_EQUAL(5, frames.count());
+    }
+
+    TEST(OneSamplePerFrame)
+    {
+        Aquila::FramesCollection frames(data, 1);
+        CHECK_EQUAL(10, frames.count());
+    }
+
+    TEST(AllSamplesPerFrame)
+    {
+        Aquila::FramesCollection frames(data, 10);
+        CHECK_EQUAL(1, frames.count());
+    }
+
+    TEST(MoreThanHalfSamplesPerFrame)
+    {
+        Aquila::FramesCollection frames(data, 7);
+        CHECK_EQUAL(1, frames.count());
+    }
+
+    TEST(TooManySamplesPerFrame)
+    {
+        Aquila::FramesCollection frames(data, 11);
+        CHECK_EQUAL(0, frames.count());
+    }
 }
