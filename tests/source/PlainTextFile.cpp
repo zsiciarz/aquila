470260b1e602115581f8f70e22079fb489649b40
Zbigniew Siciarz <antyqjon@gmail.com>
2013-02-19 00:07:33 +0100

    Tests for saving text files.

--- tests/source/PlainTextFile.cpp
+++ tests/source/PlainTextFile.cpp
@@ -1,4 +1,5 @@
 #include "aquila/global.h"
+#include "aquila/source/ArrayData.h"
 #include "aquila/source/PlainTextFile.h"
 #include "constants.h"
 #include <unittestpp.h>
@@ -25,5 +26,20 @@ SUITE(PlainTextFile)
         CHECK_CLOSE(2.0, txt.sample(1), 0.000001);
         CHECK_CLOSE(3.0, txt.sample(2), 0.000001);
         CHECK_CLOSE(3.141592, txt.sample(3), 0.000001);
+    }
+
+    TEST(SaveArray)
+    {
+        const int SIZE = 10;
+        Aquila::SampleType testArray[SIZE] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        Aquila::ArrayData<> data(testArray, SIZE, 22050);
+        Aquila::PlainTextFile::save(data, Aquila_TEST_TXTFILE_OUTPUT);
+
+        Aquila::PlainTextFile txt(Aquila_TEST_TXTFILE_OUTPUT, 22050);
+        CHECK_EQUAL(SIZE, txt.getSamplesCount());
+        for (unsigned int i = 0; i < SIZE; ++i)
+        {
+            CHECK_EQUAL(testArray[i], txt.sample(i));
+        }
     }
 }
