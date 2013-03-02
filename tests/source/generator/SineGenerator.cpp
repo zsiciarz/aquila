3d0eb437e90e4900d3f1c331ebfb9a9017d85093
Zbigniew Siciarz <antyqjon@gmail.com>
2013-03-02 23:25:05 +0100

    Initial tests for mean values of generated sine waves.

---
+++ tests/source/generator/SineGenerator.cpp
@@ -0,0 +1,24 @@
+#include "aquila/global.h"
+#include "aquila/source/generator/SineGenerator.h"
+#include <unittestpp.h>
+#include <algorithm>
+#include <cstddef>
+#include <numeric>
+
+SUITE(SineGenerator)
+{
+    // sample frequency is fixed at 1 kHz
+    Aquila::SineGenerator gen(1000);
+
+    TEST(ExactlyOnePeriod)
+    {
+        gen.setAmplitude(1).setFrequency(10).generate(100);
+        CHECK_CLOSE(0.0, Aquila::mean(gen), 0.000001);
+    }
+
+    TEST(ExactlyTenPeriods)
+    {
+        gen.setAmplitude(1).setFrequency(100).generate(100);
+        CHECK_CLOSE(0.0, Aquila::mean(gen), 0.000001);
+    }
+}
