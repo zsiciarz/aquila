f9de5c77224b72f49cebab222b6b96e52db1cabf
Zbigniew Siciarz <antyqjon@gmail.com>
2013-03-04 19:24:10 +0100

    Smoke tests for basic DFT properties.

--- tests/transform/Fft.cpp
+++ tests/transform/Fft.cpp
@@ -1,4 +1,5 @@
 #include "aquila/global.h"
+#include "aquila/source/ArrayData.h"
 #include "aquila/source/generator/SineGenerator.h"
 #include "aquila/transform/OouraFft.h"
 #include <unittestpp.h>
@@ -36,6 +37,39 @@ unsigned int findPeak(std::size_t arraySize,
 
 SUITE(Fft)
 {
+    TEST(Delta)
+    {
+        const std::size_t SIZE = 8;
+        Aquila::SampleType testArray[SIZE] = {1, 0, 0, 0, 0, 0, 0, 0};
+        Aquila::ArrayData<> data(testArray, SIZE, 22050);
+        Aquila::ComplexType* spectrum = new Aquila::ComplexType[SIZE];
+        Aquila::OouraFft fft(SIZE);
+        fft.fft(data.toArray(), spectrum);
+
+        std::for_each(spectrum, spectrum + SIZE, [](Aquila::ComplexType x) {
+            CHECK_CLOSE(1.0, std::abs(x), 0.000001);
+        });
+
+        delete [] spectrum;
+    }
+
+    TEST(ConstSignal)
+    {
+        const std::size_t SIZE = 8;
+        Aquila::SampleType testArray[SIZE] = {1, 1, 1, 1, 1, 1, 1, 1};
+        Aquila::ArrayData<> data(testArray, SIZE, 22050);
+        Aquila::ComplexType* spectrum = new Aquila::ComplexType[SIZE];
+        Aquila::OouraFft fft(SIZE);
+        fft.fft(data.toArray(), spectrum);
+
+        CHECK_CLOSE(1.0 * SIZE, std::abs(spectrum[0]), 0.000001);
+        std::for_each(spectrum + 1, spectrum + SIZE, [](Aquila::ComplexType x) {
+            CHECK_CLOSE(0.0, std::abs(x), 0.000001);
+        });
+
+        delete [] spectrum;
+    }
+
     TEST(SinePeakDetect1)
     {
         unsigned int peakPosition = findPeak(64, 8000, 1000);
