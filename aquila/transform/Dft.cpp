0e76a9c40e3b661f550d18bfdfd7d3ec087c4d7f
Zbigniew Siciarz <antyqjon@gmail.com>
2013-03-04 22:28:17 +0100

    Added a reference DFT implementation.

---
+++ aquila/transform/Dft.cpp
@@ -0,0 +1,64 @@
+/**
+ * @file Dft.cpp
+ *
+ * A reference implementation of the Discrete Fourier Transform.
+ *
+ * Note - this is a direct application of the DFT equations and as such it
+ * surely isn't optimal. The implementation here serves only as a reference
+ * model to compare with.
+ *
+ * This file is part of the Aquila DSP library.
+ * Aquila is free software, licensed under the MIT/X11 License. A copy of
+ * the license is provided with the library in the LICENSE file.
+ *
+ * @package Aquila
+ * @version 3.0.0-dev
+ * @author Zbigniew Siciarz
+ * @date 2007-2013
+ * @license http://www.opensource.org/licenses/mit-license.php MIT
+ * @since 3.0.0
+ */
+
+#include "Dft.h"
+#include <algorithm>
+#include <cmath>
+#include <complex>
+
+namespace Aquila
+{
+    /**
+     * Complex unit.
+     */
+    const ComplexType Dft::j(0, 1);
+
+    /**
+     * Applies the transformation to the signal.
+     *
+     * @param x input signal
+     * @param spectrum output spectrum
+     */
+    void Dft::fft(const SampleType x[], ComplexType spectrum[])
+    {
+        ComplexType WN = std::exp((-j) * 2.0 * M_PI / static_cast<double>(N));
+        for (unsigned int k = 0; k < N; ++k)
+        {
+            ComplexType sum(0, 0);
+            for (unsigned int n = 0; n < N; ++n)
+            {
+                sum += x[n] * std::pow(WN, n * k);
+            }
+            spectrum[k] = sum;
+        }
+    }
+
+    /**
+     * Applies the inverse transform to the spectrum.
+     *
+     * @param spectrum input spectrum
+     * @param x output signal
+     */
+    void Dft::ifft(ComplexType spectrum[], double x[])
+    {
+        // TODO
+    }
+}
---
+++ tests/transform/Dft.cpp
@@ -0,0 +1,46 @@
+#include "aquila/global.h"
+#include "aquila/source/ArrayData.h"
+#include "aquila/transform/Dft.h"
+#include <unittestpp.h>
+#include <algorithm>
+#include <cstddef>
+#include <iterator>
+#include <vector>
+
+
+SUITE(Dft)
+{
+    TEST(Delta)
+    {
+        const std::size_t SIZE = 8;
+        Aquila::SampleType testArray[SIZE] = {1, 0, 0, 0, 0, 0, 0, 0};
+        Aquila::ArrayData<> data(testArray, SIZE, 22050);
+        Aquila::ComplexType* spectrum = new Aquila::ComplexType[SIZE];
+        Aquila::Dft fft(SIZE);
+        fft.fft(data.toArray(), spectrum);
+
+        std::for_each(spectrum, spectrum + SIZE, [](Aquila::ComplexType x) {
+            CHECK_CLOSE(1.0, std::abs(x), 0.000001);
+        });
+
+        delete [] spectrum;
+    }
+
+    TEST(ConstSignal)
+    {
+        const std::size_t SIZE = 8;
+        Aquila::SampleType testArray[SIZE] = {1, 1, 1, 1, 1, 1, 1, 1};
+        Aquila::ArrayData<> data(testArray, SIZE, 22050);
+        Aquila::ComplexType* spectrum = new Aquila::ComplexType[SIZE];
+        Aquila::Dft fft(SIZE);
+        fft.fft(data.toArray(), spectrum);
+
+        CHECK_CLOSE(1.0 * SIZE, std::abs(spectrum[0]), 0.000001);
+        std::for_each(spectrum + 1, spectrum + SIZE, [](Aquila::ComplexType x) {
+            CHECK_CLOSE(0.0, std::abs(x), 0.000001);
+        });
+
+        delete [] spectrum;
+    }
+}
+
