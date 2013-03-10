5e29cc5a0cb41534fb9641e75a9424d7bce57202
Zbigniew Siciarz <antyqjon@gmail.com>
2013-03-10 13:53:15 +0100

    Cleaned up generic FFT tests.

--- tests/transform/Fft.cpp
+++ tests/transform/Fft.cpp
@@ -1,12 +1,10 @@
 #include "aquila/global.h"
-#include "aquila/source/ArrayData.h"
 #include "aquila/source/generator/SineGenerator.h"
 #include "aquila/transform/FftFactory.h"
 #include <unittestpp.h>
 #include <algorithm>
 #include <cstddef>
 #include <iterator>
-#include <vector>
 
 unsigned int findPeak(std::size_t arraySize,
                       Aquila::FrequencyType sampleFrequency,
@@ -35,40 +33,6 @@ unsigned int findPeak(std::size_t arraySize,
 
 SUITE(Fft)
 {
-    TEST(Delta)
-    {
-        const std::size_t SIZE = 8;
-        Aquila::SampleType testArray[SIZE] = {1, 0, 0, 0, 0, 0, 0, 0};
-        Aquila::ArrayData<> data(testArray, SIZE, 22050);
-        auto fft = Aquila::FftFactory::getFft(SIZE);
-        Aquila::SpectrumType spectrum = fft->fft(data.toArray());
-
-        double absSpectrum[SIZE] = {0};
-        std::transform(std::begin(spectrum), std::end(spectrum), absSpectrum, [] (Aquila::ComplexType x) {
-            return std::abs(x);
-        });
-
-        double expected[SIZE] = {1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0};
-        CHECK_ARRAY_CLOSE(expected, absSpectrum, SIZE, 0.0001);
-    }
-
-    TEST(ConstSignal)
-    {
-        const std::size_t SIZE = 8;
-        Aquila::SampleType testArray[SIZE] = {1, 1, 1, 1, 1, 1, 1, 1};
-        Aquila::ArrayData<> data(testArray, SIZE, 22050);
-        auto fft = Aquila::FftFactory::getFft(SIZE);
-        Aquila::SpectrumType spectrum = fft->fft(data.toArray());
-
-        double absSpectrum[SIZE] = {0};
-        std::transform(std::begin(spectrum), std::end(spectrum), absSpectrum, [] (Aquila::ComplexType x) {
-            return std::abs(x);
-        });
-
-        double expected[SIZE] = {SIZE * 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
-        CHECK_ARRAY_CLOSE(expected, absSpectrum, SIZE, 0.0001);
-    }
-
     TEST(SinePeakDetect1)
     {
         unsigned int peakPosition = findPeak(64, 8000, 1000);
