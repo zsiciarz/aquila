55c43c2486a3c6ecaca4f81114df28858036aa09
Zbigniew Siciarz <antyqjon@gmail.com>
2013-03-10 14:27:23 +0100

    Various test signal sizes in AquilaFft tests.

--- tests/transform/AquilaFft.cpp
+++ tests/transform/AquilaFft.cpp
@@ -9,10 +9,16 @@ SUITE(AquilaFft)
     TEST(Delta)
     {
         deltaSpectrumTest<Aquila::AquilaFft>();
+        deltaSpectrumTest<Aquila::AquilaFft, 16>();
+        deltaSpectrumTest<Aquila::AquilaFft, 128>();
+        deltaSpectrumTest<Aquila::AquilaFft, 1024>();
     }
 
     TEST(ConstSignal)
     {
         constSpectrumTest<Aquila::AquilaFft>();
+        constSpectrumTest<Aquila::AquilaFft, 16>();
+        constSpectrumTest<Aquila::AquilaFft, 128>();
+        constSpectrumTest<Aquila::AquilaFft, 1024>();
     }
 }
