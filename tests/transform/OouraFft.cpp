6d993ab1e6621c1bab8daab8902372bfe4362722
Zbigniew Siciarz <antyqjon@gmail.com>
2013-03-10 14:38:35 +0100

    OouraFft tests with various signal size.

--- tests/transform/OouraFft.cpp
+++ tests/transform/OouraFft.cpp
@@ -9,20 +9,32 @@ SUITE(OouraFft)
     TEST(Delta)
     {
         deltaSpectrumTest<Aquila::OouraFft>();
+        deltaSpectrumTest<Aquila::OouraFft, 16>();
+        deltaSpectrumTest<Aquila::OouraFft, 128>();
+        deltaSpectrumTest<Aquila::OouraFft, 1024>();
     }
 
     TEST(ConstSignal)
     {
         constSpectrumTest<Aquila::OouraFft>();
+        constSpectrumTest<Aquila::OouraFft, 16>();
+        constSpectrumTest<Aquila::OouraFft, 128>();
+        constSpectrumTest<Aquila::OouraFft, 1024>();
     }
 
     TEST(DeltaInverse)
     {
         deltaInverseTest<Aquila::OouraFft>();
+        deltaInverseTest<Aquila::OouraFft, 16>();
+        deltaInverseTest<Aquila::OouraFft, 128>();
+        deltaInverseTest<Aquila::OouraFft, 1024>();
     }
 
     TEST(ConstInverse)
     {
         constInverseTest<Aquila::OouraFft>();
+        constInverseTest<Aquila::OouraFft, 16>();
+        constInverseTest<Aquila::OouraFft, 128>();
+        constInverseTest<Aquila::OouraFft, 1024>();
     }
 }
