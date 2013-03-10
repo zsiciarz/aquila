6ff63f3df507beb7c9b8186b8c7cbb63e384fd13
Zbigniew Siciarz <antyqjon@gmail.com>
2013-03-10 14:36:56 +0100

    Tests for DFT with different signal lengths.

--- tests/transform/Dft.cpp
+++ tests/transform/Dft.cpp
@@ -9,20 +9,32 @@ SUITE(Dft)
     TEST(Delta)
     {
         deltaSpectrumTest<Aquila::Dft>();
+        deltaSpectrumTest<Aquila::Dft, 16>();
+        deltaSpectrumTest<Aquila::Dft, 128>();
+        deltaSpectrumTest<Aquila::Dft, 1024>();
     }
 
     TEST(ConstSignal)
     {
         constSpectrumTest<Aquila::Dft>();
+        constSpectrumTest<Aquila::Dft, 16>();
+        constSpectrumTest<Aquila::Dft, 128>();
+        constSpectrumTest<Aquila::Dft, 1024>();
     }
 
     TEST(DeltaInverse)
     {
         deltaInverseTest<Aquila::Dft>();
+        deltaInverseTest<Aquila::Dft, 16>();
+        deltaInverseTest<Aquila::Dft, 128>();
+        deltaInverseTest<Aquila::Dft, 1024>();
     }
 
     TEST(ConstInverse)
     {
         constInverseTest<Aquila::Dft>();
+        constInverseTest<Aquila::Dft, 16>();
+        constInverseTest<Aquila::Dft, 128>();
+        constInverseTest<Aquila::Dft, 1024>();
     }
 }
