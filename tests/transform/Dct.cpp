45c1f0c2afc7494c7196aaa5965d77915a4e3816
Zbigniew Siciarz <zbigniew.siciarz@goldenline.pl>
2013-03-15 15:16:58 +0100

    Basic tests for DCT.

---
+++ tests/transform/Dct.cpp
@@ -0,0 +1,22 @@
+#include "aquila/global.h"
+#include "aquila/transform/Dct.h"
+#include <unittestpp.h>
+#include <cstddef>
+#include <vector>
+
+
+SUITE(Dct)
+{
+    TEST(AlternatingOnes)
+    {
+        const std::size_t SIZE = 4;
+        const double testArray[SIZE] = {1.0, -1.0, 1.0, -1.0};
+        std::vector<double> vec(testArray, testArray + SIZE);
+
+        Aquila::Dct dct;
+        auto output = dct.dct(vec, SIZE);
+
+        double expected[SIZE] = {0.0, 0.76536686, 0.0, 1.84775907};
+        CHECK_ARRAY_CLOSE(expected, output, SIZE, 0.0001);
+    }
+}
