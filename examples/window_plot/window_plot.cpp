278a3708ad114e33e0582bdc1faa2f4bf71ff995
Zbigniew Siciarz <antyqjon@gmail.com>
2013-03-24 15:56:20 +0100

    Updated window_plot example to use new-style window classes.

--- examples/window_plot/window_plot.cpp
+++ examples/window_plot/window_plot.cpp
@@ -1,24 +1,39 @@
-#include "aquila/transform/Window.h"
+#include "aquila/source/window/BarlettWindow.h"
+#include "aquila/source/window/BlackmanWindow.h"
+#include "aquila/source/window/FlattopWindow.h"
+#include "aquila/source/window/HammingWindow.h"
+#include "aquila/source/window/HannWindow.h"
+#include "aquila/source/window/RectangularWindow.h"
 #include "aquila/tools/TextPlot.h"
 
 int main(int argc, char *argv[])
 {
-    Aquila::WindowType types[] = {Aquila::WIN_RECT, Aquila::WIN_HAMMING,
-                                  Aquila::WIN_HANN, Aquila::WIN_BARLETT,
-                                  Aquila::WIN_BLACKMAN, Aquila::WIN_FLATTOP};
-    const char* names[] = {"Rectangular", "Hamming",
-                           "Hann", "Barlett",
-                           "Blackman", "Flat-top"};
-    const std::size_t length = 65;
-    int num = sizeof(types) / sizeof(Aquila::WindowType);
-
+    const std::size_t LENGTH = 65;
     Aquila::TextPlot plot;
-    for (int i = 0; i < num; ++i)
-    {
-        Aquila::Window window(types[i], length);
-        plot.setTitle(names[i]);
-        plot.plot(window.getData());
-    }
+
+    Aquila::BarlettWindow barlett(LENGTH);
+    plot.setTitle("Barlett");
+    plot.plot(barlett);
+
+    Aquila::BlackmanWindow blackman(LENGTH);
+    plot.setTitle("Blackman");
+    plot.plot(blackman);
+
+    Aquila::FlattopWindow flattop(LENGTH);
+    plot.setTitle("Flattop");
+    plot.plot(flattop);
+
+    Aquila::HammingWindow hamming(LENGTH);
+    plot.setTitle("Hamming");
+    plot.plot(hamming);
+
+    Aquila::HannWindow hann(LENGTH);
+    plot.setTitle("Hann");
+    plot.plot(hann);
+
+    Aquila::RectangularWindow rect(LENGTH);
+    plot.setTitle("Rectangular");
+    plot.plot(rect);
 
     return 0;
 }
