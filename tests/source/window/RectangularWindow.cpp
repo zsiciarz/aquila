3e2aed4208b6aeae280f76f7c5f65362d45ec6fa
Zbigniew Siciarz <antyqjon@gmail.com>
2013-03-24 15:15:56 +0100

    Added RectangularWindow class.

---
+++ tests/source/window/RectangularWindow.cpp
@@ -0,0 +1,26 @@
+#include "aquila/global.h"
+#include "aquila/source/window/RectangularWindow.h"
+#include "aquila/source/generator/SquareGenerator.h"
+#include <unittestpp.h>
+#include <cstddef>
+
+SUITE(RectangularWindow)
+{
+    TEST(SampleValue)
+    {
+        const std::size_t SIZE = 4;
+        Aquila::RectangularWindow w(SIZE);
+        Aquila::SampleType expected[SIZE] = {1.0, 1.0, 1.0, 1.0};
+        CHECK_ARRAY_CLOSE(expected, w.toArray(), SIZE, 0.000001);
+    }
+
+    TEST(Multiplication)
+    {
+        const std::size_t SIZE = 1024;
+        Aquila::SquareGenerator generator(44100);
+        generator.setFrequency(1000).setAmplitude(500).generate(SIZE);
+        Aquila::RectangularWindow w(SIZE);
+        auto result = generator * w;
+        CHECK_ARRAY_CLOSE(generator.toArray(), result.toArray(), SIZE, 0.00001);
+    }
+}
