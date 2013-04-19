939e71f3c756fa336fffa1689837205dd32c4766
Zbigniew Siciarz <antyqjon@gmail.com>
2013-04-19 22:52:02 +0200

    Added test for constant DCT input.

--- tests/transform/Dct.cpp
+++ tests/transform/Dct.cpp
@@ -19,4 +19,17 @@ SUITE(Dct)
         double expected[SIZE] = {0.0, 0.76536686, 0.0, 1.84775907};
         CHECK_ARRAY_CLOSE(expected, output, SIZE, 0.0001);
     }
+
+    TEST(ConstantInput)
+    {
+        const std::size_t SIZE = 4;
+        const double testArray[SIZE] = {1.0, 1.0, 1.0, 1.0};
+        std::vector<double> vec(testArray, testArray + SIZE);
+
+        Aquila::Dct dct;
+        auto output = dct.dct(vec, SIZE);
+
+        double expected[SIZE] = {2.0, 0.0, 0.0, 0.0};
+        CHECK_ARRAY_CLOSE(expected, output, SIZE, 0.0001);
+    }
 }
