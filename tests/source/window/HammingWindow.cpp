ce7737779cf96eaa83e236d029523a1e0e98149f
Zbigniew Siciarz <antyqjon@gmail.com>
2013-04-19 23:17:55 +0200

    Test for basic property of Hamming window (non-zero at ends).

---
+++ tests/source/window/HammingWindow.cpp
@@ -0,0 +1,15 @@
+#include "aquila/global.h"
+#include "aquila/source/window/HammingWindow.h"
+#include <unittestpp.h>
+#include <cstddef>
+
+SUITE(HammingWindow)
+{
+    TEST(NonZeroAtEnds)
+    {
+        const std::size_t SIZE = 4;
+        Aquila::HammingWindow w(SIZE);
+        CHECK(w.sample(0) > 0.000001);
+        CHECK(w.sample(SIZE - 1) > 0.000001);
+    }
+}
