f506932813dab634a093041e7183ce1d35c355e3
Zbigniew Siciarz <antyqjon@gmail.com>
2013-04-20 18:50:49 +0200

    More informative doc comment for Mfcc class.

--- aquila/transform/Mfcc.h
+++ aquila/transform/Mfcc.h
@@ -30,7 +30,26 @@ namespace Aquila
     /**
      * The Mfcc class implements calculation of MFCC features from input signal.
      *
-     * MFCC coefficients are commonly used in speech recognition.
+     * MFCC coefficients are commonly used in speech recognition. The common
+     * workflow is to split input signal in frames of equal length and apply
+     * MFCC calculation to each frame individually. Hence a few assumptions
+     * were made here:
+     *
+     * - a single Mfcc instance can be used only to process signals of equal
+     *   length, for example consecutive frames
+     * - if you need to handle signals of various lengths, just create new
+     *   Mfcc object per each signal source
+     *
+     * The code below is a simplest possible example of how to calculate MFCC
+     * for each frame of input signal.
+     *
+     * FramesCollection frames(data, FRAME_SIZE);
+     * Mfcc mfcc(FRAME_SIZE);
+     * for (Frame& frame : frames) {
+     *    auto mfccValues = mfcc.calculate(frame);
+     *    // do something with the calculated values
+     * }
+     *
      */
     class AQUILA_EXPORT Mfcc
     {
