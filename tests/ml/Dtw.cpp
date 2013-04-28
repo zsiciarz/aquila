20d2c15ae55c09a67f7c8a76b17c2046c2273b99
Zbigniew Siciarz <antyqjon@gmail.com>
2013-04-28 22:35:30 +0200

    Test for DTW zero distance along diagonal

--- tests/ml/Dtw.cpp
+++ tests/ml/Dtw.cpp
@@ -23,5 +23,35 @@ SUITE(Dtw)
 
         CHECK_CLOSE(0.0, distance, 0.000001);
     }
+
+    TEST(ZeroDiagonalDistance)
+    {
+        const std::size_t SIZE = 3;
+        double arr1[SIZE] = {0, 0, 0}, arr2[SIZE] = {1, 1, 1};
+        std::vector<double> zeros(arr1, arr1 + SIZE), ones(arr2, arr2 + SIZE);
+
+        Aquila::DtwDataType from, to;
+        from.push_back(zeros);
+        from.push_back(ones);
+        from.push_back(ones);
+        to.push_back(zeros);
+        to.push_back(ones);
+        to.push_back(ones);
+
+        /**
+         * this will give the following local Manhattan distances:
+         *
+         * 3 0 0
+         * 3 0 0
+         * 0 3 3
+         *
+         * and the lowest-cost path will be on the diagonal.
+         */
+
+        Aquila::Dtw dtw(Aquila::manhattanDistance);
+        double distance = dtw.getDistance(from, to);
+
+        CHECK_CLOSE(0.0, distance, 0.000001);
+    }
 }
 
