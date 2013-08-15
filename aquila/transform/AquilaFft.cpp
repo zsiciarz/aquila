e59b43f3ee9aef10234996e4a23ca24ba02a6d74
Zbigniew Siciarz <antyqjon@gmail.com>
2013-08-16 01:23:42 +0200

    Removed TODO note.

--- aquila/transform/AquilaFft.cpp
+++ aquila/transform/AquilaFft.cpp
@@ -103,7 +103,6 @@ namespace Aquila
     /**
      * Applies the inverse transform to the spectrum.
      *
-     * @todo replace with a real IFFT implementation
      * @param spectrum input spectrum
      * @param x output signal
      */
