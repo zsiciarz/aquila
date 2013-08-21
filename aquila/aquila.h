35b34eeb2f16d42e4e59575222a274873f69235e
Zbigniew Siciarz <antyqjon@gmail.com>
2013-08-21 23:11:58 +0200

    Added a master header - aquila.h

---
+++ aquila/aquila.h
@@ -0,0 +1,30 @@
+/**
+ * @file aquila.h
+ *
+ * Library "master" header - includes all component headers.
+ *
+ * This file is part of the Aquila DSP library.
+ * Aquila is free software, licensed under the MIT/X11 License. A copy of
+ * the license is provided with the library in the LICENSE file.
+ *
+ * @package Aquila
+ * @version 3.0.0-dev
+ * @author Zbigniew Siciarz
+ * @date 2007-2013
+ * @license http://www.opensource.org/licenses/mit-license.php MIT
+ * @since 3.0.0
+ */
+
+#ifndef AQUILA_H
+#define AQUILA_H
+
+#include "global.h"
+#include "Exceptions.h"
+#include "functions.h"
+#include "source.h"
+#include "transform.h"
+#include "filter.h"
+#include "ml.h"
+#include "tools.h"
+
+#endif // AQUILA_H
