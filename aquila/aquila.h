6f5d6b15160c6b4349eda0e2baec9b099e647448
Zbigniew Siciarz <antyqjon@gmail.com>
2013-10-13 22:52:06 +0200

    Initial mainpage (references #26).

--- aquila/aquila.h
+++ aquila/aquila.h
@@ -13,6 +13,24 @@
  * @date 2007-2013
  * @license http://www.opensource.org/licenses/mit-license.php MIT
  * @since 3.0.0
+ *
+ * @mainpage
+ *
+ * @section what-is-aquila What is Aquila?
+ * Aquila is an open source and cross-platform DSP (Digital Signal Processing)
+ * library for C++11.
+ *
+ * Aquila provides a set of classes for common DSP operations, such as FFT, DCT,
+ * Mel-frequency filtering, calculating spectrograms etc. It supports reading
+ * and writing signals in various formats, such as raw binary files, text files
+ * or WAVE audio recordings.
+ *
+ * @section motivation Motivation
+ * The initial goal of this project was to develop computer software capable
+ * of recognizing birds' songs. Since then the library was redesigned and
+ * extended with more general DSP tools. There are still a few major
+ * shortcomings, for example the lack of general purpose filter classes, but
+ * hopefully this will change soon.
  */
 
 #ifndef AQUILA_H
