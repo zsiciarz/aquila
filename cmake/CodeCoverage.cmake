e161a3a28ebda6d62a89e6147a0df82c454ed25e
Zbigniew Siciarz <antyqjon@gmail.com>
2014-01-20 22:02:53 +0100

    Exclude UnitTest++ code from coverage.

--- cmake/CodeCoverage.cmake
+++ cmake/CodeCoverage.cmake
@@ -62,7 +62,7 @@ FUNCTION(SETUP_TARGET_FOR_COVERAGE _targetname _testrunner _outputname)
 		
 		# Capturing lcov counters and generating report
 		COMMAND ${LCOV_PATH} --directory . --capture --output-file ${_outputname}.info
-		COMMAND ${LCOV_PATH} --remove ${_outputname}.info 'tests/*' '/usr/*' --output-file ${_outputname}.info.cleaned
+		COMMAND ${LCOV_PATH} --remove ${_outputname}.info 'tests/*' 'lib/unittestpp/*' '/usr/*' --output-file ${_outputname}.info.cleaned
 		COMMAND ${GENHTML_PATH} -o ${_outputname} ${_outputname}.info.cleaned
 		COMMAND ${CMAKE_COMMAND} -E remove ${_outputname}.info ${_outputname}.info.cleaned
 		
