efdf716ec22883bc9e6fa4572ccf2e9ff0f2c423
Zbigniew Siciarz <antyqjon@gmail.com>
2014-08-08 18:59:36 +0200

    Added basic tests for all remaining functions in functions.h

--- tests/functions.cpp
+++ tests/functions.cpp
@@ -12,6 +12,60 @@ SUITE(Functions)
     double arr2[SIZE] = {1, 2, 3};
     std::vector<double> v2(arr2, arr2 + SIZE);
 
+    TEST(DecibelConversion)
+    {
+        double decibel = Aquila::dB(10.0);
+        CHECK_CLOSE(20.0, decibel, 0.000001);
+    }
+
+    TEST(ComplexDecibelConversion)
+    {
+        double decibel = Aquila::dB(Aquila::ComplexType(0.0, 10.0));
+        CHECK_CLOSE(20.0, decibel, 0.000001);
+    }
+
+    TEST(ReferenceDecibelConversion)
+    {
+        double decibel = Aquila::dB(1000.0, 10.0);
+        CHECK_CLOSE(40.0, decibel, 0.000001);
+    }
+
+    TEST(ClampToMax)
+    {
+        double value = Aquila::clamp(5.0, 9.0, 6.0);
+        CHECK_CLOSE(6.0, value, 0.000001);
+    }
+
+    TEST(ClampToMin)
+    {
+        double value = Aquila::clamp(5.0, 1.0, 6.0);
+        CHECK_CLOSE(5.0, value, 0.000001);
+    }
+
+    TEST(ClampUnchanged)
+    {
+        double value = Aquila::clamp(5.0, 5.5, 6.0);
+        CHECK_CLOSE(5.5, value, 0.000001);
+    }
+
+    TEST(RandomRange)
+    {
+        for (std::size_t i = 0; i < 1000; ++i)
+        {
+            int x = Aquila::random(1, 2);
+            CHECK_EQUAL(1, x);
+        }
+    }
+
+    TEST(RandomDoubleRange)
+    {
+        for (std::size_t i = 0; i < 1000; ++i)
+        {
+            double x = Aquila::randomDouble();
+            CHECK(x < 1.0);
+        }
+    }
+
     TEST(EuclideanDistanceToItself)
     {
         double distance = Aquila::euclideanDistance(v1, v1);
