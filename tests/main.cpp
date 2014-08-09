a969e35e746904de4fa3796e91eda0c6c742f024
Zbigniew Siciarz <antyqjon@gmail.com>
2014-08-09 23:18:17 +0200

    More verbose test runner output.

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,6 +1,22 @@
-#include <unittestpp.h>
+#include <iostream>
+#include "src/TestDetails.h"
+#include "src/TestReporterStdout.h"
+#include "src/TestRunner.h"
+#include "src/Test.h"
+
+
+class AquilaTestReporter : public UnitTest::TestReporterStdout
+{
+    void ReportTestStart(UnitTest::TestDetails const& test)
+    {
+        std::cout << test.testName << std::endl;
+    }
+};
+
 
 int main()
 {
-    return UnitTest::RunAllTests();
+    AquilaTestReporter reporter;
+    UnitTest::TestRunner runner(reporter);
+    return runner.RunTestsIf(UnitTest::Test::GetTestList(), NULL, UnitTest::True(), 0);
 }
