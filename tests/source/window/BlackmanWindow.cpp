b5c7f8b27209870d6f8963f26e1f9902e314e82e
Zbigniew Siciarz <antyqjon@gmail.com>
2014-08-10 14:44:13 +0200

    Basic tests of Blackman window properties.

---
+++ tests/source/window/BlackmanWindow.cpp
@@ -0,0 +1,28 @@
+#include "aquila/global.h"
+#include "aquila/source/window/BlackmanWindow.h"
+#include "aquila/source/generator/SquareGenerator.h"
+#include "UnitTest++/UnitTest++.h"
+#include <cstddef>
+
+SUITE(BlackmanWindow)
+{
+    TEST(ZeroAtEnds)
+    {
+        const std::size_t SIZE = 4;
+        Aquila::BlackmanWindow w(SIZE);
+        CHECK_CLOSE(0.0, w.sample(0), 0.000001);
+        CHECK_CLOSE(0.0, w.sample(SIZE - 1), 0.000001);
+    }
+
+    TEST(Multiplication)
+    {
+        const std::size_t SIZE = 1024;
+        Aquila::SquareGenerator generator(44100);
+        generator.setFrequency(1000).setAmplitude(500).generate(SIZE);
+        Aquila::BlackmanWindow w(SIZE);
+        auto result = generator * w;
+        CHECK_CLOSE(0.0, result.sample(0), 0.000001);
+        CHECK_CLOSE(0.0, result.sample(SIZE - 1), 0.000001);
+    }
+}
+
