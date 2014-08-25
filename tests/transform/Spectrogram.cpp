f4d9630324958c91e3360ad5b21eae26ae692c64
Zbigniew Siciarz <antyqjon@gmail.com>
2014-08-25 22:12:37 +0200

    Initial tests for Spectrogram class.

---
+++ tests/transform/Spectrogram.cpp
@@ -0,0 +1,55 @@
+#include "aquila/global.h"
+#include "aquila/source/generator/SineGenerator.h"
+#include "aquila/source/FramesCollection.h"
+#include "aquila/transform/Spectrogram.h"
+#include "UnitTest++/UnitTest++.h"
+
+
+SUITE(Spectrogram)
+{
+    const std::size_t SIZE = 512;
+    Aquila::FrequencyType sampleFrequency = 1024;
+
+    TEST(Dimensions)
+    {
+        Aquila::SineGenerator generator(sampleFrequency);
+        generator.setFrequency(32).setAmplitude(1).generate(SIZE);
+
+        // divide into 4 frames, 128 samples each
+        Aquila::FramesCollection frames(generator, SIZE / 4);
+        CHECK_EQUAL(4, frames.count());
+
+        Aquila::Spectrogram spectrogram(frames);
+        CHECK_EQUAL(frames.count(), spectrogram.getFrameCount());
+        CHECK_EQUAL(frames.frame(0).length(), spectrogram.getSpectrumSize());
+    }
+
+    TEST(SpectrumPeaks)
+    {
+        Aquila::FrequencyType testFrequency = 32;
+        Aquila::SineGenerator generator(sampleFrequency);
+        generator.setFrequency(testFrequency).setAmplitude(1).generate(SIZE);
+
+        // divide into 4 frames, 128 samples each
+        const std::size_t samplesPerFrame = SIZE / 4;
+        Aquila::FramesCollection frames(generator, samplesPerFrame);
+        CHECK_EQUAL(4, frames.count());
+
+        Aquila::Spectrogram spectrogram(frames);
+
+        // check all frames for a single spectral peak coming from the sine
+        std::size_t halfLength = samplesPerFrame / 2;
+        std::size_t expectedPeak = testFrequency * samplesPerFrame / sampleFrequency;
+        std::vector<double> expected(halfLength);
+        expected[expectedPeak] = halfLength * 1.0;
+        for (std::size_t x = 0; x < spectrogram.getFrameCount(); ++x)
+        {
+            std::vector<double> absSpectrum(halfLength);
+            for (std::size_t y = 0; y < halfLength; ++y)
+            {
+                absSpectrum[y] = std::abs(spectrogram.getPoint(x, y));
+            }
+            CHECK_ARRAY_CLOSE(expected, absSpectrum, halfLength, 0.00001);
+        }
+    }
+}
