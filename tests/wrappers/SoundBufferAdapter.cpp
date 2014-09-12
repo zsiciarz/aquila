f118cb417ae9163674d0a89777489a73bc6459c0
Zbigniew Siciarz <antyqjon@gmail.com>
2014-09-12 21:53:32 +0200

    Check that sample frequencies agree between original generator and buffer.

--- tests/wrappers/SoundBufferAdapter.cpp
+++ tests/wrappers/SoundBufferAdapter.cpp
@@ -12,4 +12,12 @@ SUITE(SoundBufferAdapter)
         Aquila::SoundBufferAdapter buffer(generator);
         CHECK_EQUAL(generator.length(), buffer.getSampleCount());
     }
+
+    TEST(SampleFrequency)
+    {
+        Aquila::SineGenerator generator(128);
+        generator.setAmplitude(1).setFrequency(8).generate(64);
+        Aquila::SoundBufferAdapter buffer(generator);
+        CHECK_EQUAL(static_cast<unsigned int>(generator.getSampleFrequency()), buffer.getSampleRate());
+    }
 }
