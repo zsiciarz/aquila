8c956be9b33cf6a74ca88254b587930612118460
Zbigniew Siciarz <antyqjon@gmail.com>
2014-09-12 22:35:24 +0200

    Another DCT test to hit the cosine cache.

--- tests/transform/Dct.cpp
+++ tests/transform/Dct.cpp
@@ -32,4 +32,18 @@ SUITE(Dct)
         double expected[SIZE] = {2.0, 0.0, 0.0, 0.0};
         CHECK_ARRAY_CLOSE(expected, output, SIZE, 0.0001);
     }
+
+    TEST(UseCachedCosines)
+    {
+        const std::size_t SIZE = 4;
+        const double testArray[SIZE] = {1.0, 1.0, 1.0, 1.0};
+        std::vector<double> vec(testArray, testArray + SIZE);
+
+        Aquila::Dct dct;
+        auto output = dct.dct(vec, SIZE);
+        auto output2 = dct.dct(vec, SIZE);
+
+        double expected[SIZE] = {2.0, 0.0, 0.0, 0.0};
+        CHECK_ARRAY_CLOSE(expected, output2, SIZE, 0.0001);
+    }
 }
