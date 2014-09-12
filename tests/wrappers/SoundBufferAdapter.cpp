98d494eb203fd71336f74ec5f0e657487875c921
Zbigniew Siciarz <antyqjon@gmail.com>
2014-09-12 21:56:33 +0200

    Tests for default and copy constructors.

--- tests/wrappers/SoundBufferAdapter.cpp
+++ tests/wrappers/SoundBufferAdapter.cpp
@@ -5,6 +5,12 @@
 
 SUITE(SoundBufferAdapter)
 {
+    TEST(Empty)
+    {
+        Aquila::SoundBufferAdapter buffer;
+        CHECK_EQUAL(0u, buffer.getSampleCount());
+    }
+
     TEST(NumSamples)
     {
         Aquila::SineGenerator generator(128);
@@ -19,5 +25,14 @@ SUITE(SoundBufferAdapter)
         generator.setAmplitude(1).setFrequency(8).generate(64);
         Aquila::SoundBufferAdapter buffer(generator);
         CHECK_EQUAL(static_cast<unsigned int>(generator.getSampleFrequency()), buffer.getSampleRate());
+    }
+
+    TEST(Copy)
+    {
+        Aquila::SineGenerator generator(128);
+        generator.setAmplitude(1).setFrequency(8).generate(64);
+        Aquila::SoundBufferAdapter buffer(generator);
+        Aquila::SoundBufferAdapter buffer2(buffer);
+        CHECK_EQUAL(generator.length(), buffer2.getSampleCount());
     }
 }
