1077138ea81e5aab46b4c506a61f53337a7c711b
Zbigniew Siciarz <antyqjon@gmail.com>
2014-09-13 16:59:24 +0200

    Refactored magic number into a constant.

--- tests/transform/Mfcc.cpp
+++ tests/transform/Mfcc.cpp
@@ -9,14 +9,15 @@ SUITE(Mfcc)
 {
     TEST(Sine9Coeffs)
     {
+        const std::size_t NUM_FEATURES = 9;
         Aquila::SineGenerator generator(2048);
         generator.setAmplitude(1).setFrequency(128).generate(2048);
 
         Aquila::Mfcc mfcc(generator.getSamplesCount());
-        auto mfccValues = mfcc.calculate(generator, 9);
-        Aquila::SampleType expected[9] = {
+        auto mfccValues = mfcc.calculate(generator, NUM_FEATURES);
+        Aquila::SampleType expected[NUM_FEATURES] = {
             418.046, 200.327, 35.796, 485.892, 311.312, -111.424, 214.684, 311.887, -194.111
         };
-        CHECK_ARRAY_CLOSE(expected, mfccValues, 9, 0.001);
+        CHECK_ARRAY_CLOSE(expected, mfccValues, NUM_FEATURES, 0.001);
     }
 }
